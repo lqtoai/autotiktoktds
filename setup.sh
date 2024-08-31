59590982affff052dcc5fd6b6ed791f2ab6244ed
lqtoai <168071811+lqtoai@users.noreply.github.com>
2024-08-31 11:45:21 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,13 @@
+home='/data/data/com.termux/files/'
+printf '\n\033[1;32m Tiến Hành Cài Dữ Liệu\n\n'
+termux-setup-storage
+apt update
+apt upgrade
+printf '\n\033[1;32m Tiến Hành Cài Python\n\n'
+pkg install python
+pip install --upgrade pip
+pip install requests
+curl -s https://lyquangtoai.shop/help/lqtoai.py -o $home/usr/bin/lqtoai_autotds
+chmod 777 $home/usr/bin/lqtoai_autotds
 
+printf '\n\033[1;32m Gõ \033[1;33mlqtoai_autotds \033[1;32mĐể Vào Tool <3\n\n'
