b4ef5f0875a0057d2c9fe56b41fdae21d9f4b4a5
lqtoai <168071811+lqtoai@users.noreply.github.com>
2024-08-31 13:09:01 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ pkg install git
 pkg install python
 pip install --upgrade pip
 pip install requests
-curl -s https://lyquangtoai.shop/help/lqtoai.py -o $home/usr/bin/lqtoai_autotds
+curl -s https://raw.githubusercontent.com/lqtoai/autotiktoktds/main/lqtoai -o $home/usr/bin/lqtoai_autotds
 chmod 777 $home/usr/bin/lqtoai_autotds
 
 printf '\n\033[1;32m Gõ \033[1;33mlqtoai_autotds \033[1;32mĐể Vào Tool <3\n\n'
